826c631ab76f1ceb63662d600a32313996f14b03
Sumit <sumitgarg380@gmail.com>
2024-06-25 13:07:40 +0530

    Fixed email

--- lib/handlereq.ts
+++ lib/handlereq.ts
@@ -3,10 +3,8 @@ import axios from "axios";
 export const handlePostRequest = async (data: any) => {
   try {
     const response = await axios.post('/api/contact', data);
-    // Handle the response here
     console.log(response.data);
   } catch (error) {
-    // Handle the error here
-    console.error(error);
+    console.log(error);
   }
 };
